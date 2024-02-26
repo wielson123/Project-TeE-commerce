6a042b247949329503c7aecbb4997ddfc46c77a4
Bert Gryson <bert.gryson@gmail.com>
2024-02-26 12:56:31 +0100

    letsgo

--- Client/app/src/config.js
+++ Client/app/src/config.js
@@ -1,3 +1,9 @@
-const URL = `http://localhost:5010`;
+// const URL = `http://localhost:5010`;
 
+// const URL =
+//   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+//     ? "http://localhost:3030"
+//     : "YourProductionURL";
+
+const URL = "https://good-gold-jackrabbit-wear.cyclic.app/";
 export { URL };
